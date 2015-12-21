3c2c8defac227c48d85bf7ee5cd1a554f326d5fc
Jonathan Medding <jonmedding+github@gmail.com>
2015-12-21 10:37:23 +0100

    Add install_centos.sh
    install script for centos or RHELg

---
+++ install_centos.sh
@@ -0,0 +1,9 @@
+sudo yum -y update && sudo yum -y upgrade
+curl -sSL https://get.docker.com | sh
+_user="$(id -u -n)"
+sudo usermod -aG docker $_user
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.4.2/docker-compose-`uname -s`-`uname -m` > docker-compose
+
+sudo mv docker-compose /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
