1ac1349b4aed8eb5c66ab9410ec48cca5c208007
Jonathan Medding <jonmedding+github@gmail.com>
2015-12-21 17:13:11 +0100

    Update install_centos.sh

--- install_centos.sh
+++ install_centos.sh
@@ -7,11 +7,13 @@ sudo usermod -aG docker $_user
 
 echo "user '$_user' added to docker group - no need to use sudo with this user for docker commands"
 
+
 sudo curl -L https://github.com/docker/compose/releases/download/1.4.2/docker-compose-`uname -s`-`uname -m` > docker-compose
 
 sudo mv docker-compose /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo yum -y install net-tools
-echo "\n"
-echo "run 'ifconfig' to determine this machines IP address"
+
+echo "********* IMPORTANT *****************"
+echo "You must logout and back in. Try 'exit' on the command line."
