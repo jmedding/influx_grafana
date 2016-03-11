00744265b008c0882c446b33bb3957e10633c17c
Jonathan Medding <jonmedding+github@gmail.com>
2016-03-11 11:23:40 +0100

    Added some cli output

--- install_centos.sh
+++ install_centos.sh
@@ -19,6 +19,7 @@ curl -sSL https://get.docker.com | sh
 sudo service docker start
 
 _user="$(id -u -n)"
+echo "*******Setting up docker group membership for user: $_user"
 #below command seems to be failing on install. Check groups and run manually if needed
 sudo usermod -aG docker $_user
 echo "user '$_user' added to docker group - no need to use sudo with this user for docker commands"
